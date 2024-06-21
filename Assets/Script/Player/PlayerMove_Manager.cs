bf61b6d230457517eb300b2f0bf3c209fae2f34d
Offline <yllow6@naver.com>
2024-06-21 22:03:38 +0900

    플레이어 움직임

--- Assets/Script/Player/PlayerMove_Manager.cs
+++ Assets/Script/Player/PlayerMove_Manager.cs
@@ -5,16 +5,17 @@ using Sirenix.OdinInspector;
 
 public class PlayerMove_Manager : MonoBehaviour
 {
-    [LabelText("캐릭터 컨트롤러")] private CharacterController _characterController;
+    [LabelText("캐릭터 리지드바디")] private Rigidbody2D _rigid;
 
     [LabelText("플레이어 이동속독")] private float _moveSpeed => GameSystem_Manager.Instance.player_MoveSpeed;
     [LabelText("좌우 입력키 방향값")] private float _horizontal;
-    [LabelText("플레이어 이동 방향 벡터")] private Vector3 _playerMoveVec;
+    [LabelText("플레이어 이동 방향 벡터")] private Vector2 _playerMoveInput;
+    [LabelText("플레이어 이동 방향 벡터 * 스피드")] private Vector2 _playerMoveVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        this._characterController = gameObject.GetComponent<CharacterController>();
+        this._rigid = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,12 +24,16 @@ public class PlayerMove_Manager : MonoBehaviour
         this.Move_PlayerMoving_Func();
     }
 
+    private void FixedUpdate()
+    {
+        this._rigid.MovePosition(this._rigid.position + this._playerMoveVelocity * Time.fixedDeltaTime);
+    }
+
     private void Move_PlayerMoving_Func()
     {
         this._horizontal = Input.GetAxis("Horizontal");
 
-        this._playerMoveVec = transform.TransformDirection(new Vector3(this._horizontal, 0, 0));
-
-        this._characterController.Move(this._playerMoveVec * Time.deltaTime * this._moveSpeed);
+        this._playerMoveInput = new Vector2(this._horizontal, 0);
+        this._playerMoveVelocity = _playerMoveInput * _moveSpeed;
     }
 }
