a9b352e551385fa5e87ec7ec41711d7eedf98f03
Offline <yllow6@naver.com>
2024-06-22 19:13:31 +0900

    플레이어 애니메이션 추가 + 동작 수정 + 대쉬 추가

--- Assets/2_Scripts/Player/PlayerMove_Script.cs
+++ Assets/2_Scripts/Player/PlayerMove_Script.cs
@@ -6,85 +6,108 @@ using Sirenix.OdinInspector;
 public class PlayerMove_Script : MonoBehaviour
 {
     [SerializeField, LabelText("캐치포인트 오브젝트")] private Transform _chtchPointTr;
+    [SerializeField, LabelText("캐릭터 스프라이트 렌더러")] private SpriteRenderer _playerSpriteRenderer;
+
+    [SerializeField, LabelText("플레이어 슬라이딩 가속도")] private int _moveboost;
 
     [LabelText("캐릭터 리지드바디")] private Rigidbody2D _rigid;
     [LabelText("캐릭터 애니메이터")] private Animator _anim;
-    [LabelText("플레이어 이동속독")] private float _moveSpeed => PlayerSystem_Manager.Instance.player_MoveSpeed;
+    [LabelText("플레이어 이동속도")] private float _moveSpeed => PlayerSystem_Manager.Instance.player_MoveSpeed;
     [LabelText("좌우 입력키 방향값")] private float _horizontal;
     [LabelText("플레이어 이동 방향 벡터")] private Vector2 _playerMoveInput;
     [LabelText("플레이어 이동 방향 벡터 * 스피드")] private Vector2 _playerMoveVelocity;
+    [LabelText("이동 금지")] private bool is_MoveOn = true;
+    [LabelText("슬라이딩 중")] private bool is_SlidingOn = false;
+
+    [LabelText("플레이어 스케일 초기값")] private Vector3 _playerStartScale;
+
     public void Start_PlayerMove_Func()
     {
         this._rigid = gameObject.GetComponent<Rigidbody2D>();
         this._anim = gameObject.GetComponent<Animator>();
+
+        this._playerStartScale = this.transform.localScale;
     }
 
     void Update()
     {
         this.Move_InputKey_Func();
         this.Move_PlayerMoving_Func();
-        this.Click_PlayerSliding_Func();
-        this.Click_PlayerCatch_Func();
     }
 
     private void FixedUpdate()
     {
-        this._rigid.MovePosition(this._rigid.position + this._playerMoveVelocity * Time.fixedDeltaTime);
+        if (this.is_MoveOn == true)
+            this._rigid.MovePosition(this._rigid.position + this._playerMoveVelocity * Time.fixedDeltaTime);
+        else if (this.is_SlidingOn == true)
+            this._rigid.velocity = this._playerMoveInput.normalized * (this._moveSpeed * _moveboost) * Time.fixedDeltaTime;
+        else
+            this._rigid.velocity = Vector2.zero;
     }
 
     private void Move_InputKey_Func()
     {
-        if(Input.GetKeyDown(KeyCode.X) == true)
+        if (this.is_SlidingOn == true)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.X) == true)
         {
             this._anim.SetBool("Catch_On", true);
+            this.is_MoveOn = false;
         }
 
         if (Input.GetKeyUp(KeyCode.X) == true)
         {
             this._anim.SetBool("Catch_On", false);
+            this.is_MoveOn = true;
         }
 
         if (Input.GetKeyDown(KeyCode.Z) == true)
         {
-            //this._anim.SetBool("Catch_On", true);
+            this._anim.SetTrigger("Silde_On");
+            this.is_SlidingOn = true;
         }
-
-        if (Input.GetKeyUp(KeyCode.Z) == true)
-        {
-            //this._anim.SetBool("Catch_On", false);
-        }
-
     }
 
     private void Move_PlayerMoving_Func()
     {
-        this._horizontal = Input.GetAxis("Horizontal");
+        if (this.is_MoveOn == false || this.is_SlidingOn == true)
+            return;
 
-        this._anim.SetFloat("Move", this._horizontal);
+        this._horizontal = Input.GetAxisRaw("Horizontal");
+
+        if(0.1f < this._horizontal)
+        {
+            this._anim.SetFloat("Move", this._horizontal);
+            if (0.0f < this._playerStartScale.x)
+                this._playerStartScale.x *= -1;
+
+            this.transform.localScale = this._playerStartScale;
+
+
+        }
+        else if(this._horizontal < -0.1f)
+        {
+            this._anim.SetFloat("Move", this._horizontal * -1);
+            if (this._playerStartScale.x < 0.0f)
+                this._playerStartScale.x *= -1;
+
+            this.transform.localScale = this._playerStartScale;
+        }
+        else
+        {
+            this._anim.SetFloat("Move", 0);
+        }
 
         this._playerMoveInput = new Vector2(this._horizontal, 0);
         this._playerMoveVelocity = _playerMoveInput * _moveSpeed;
     }
 
-    private void Click_PlayerSliding_Func()
+    public void Call_InputSliding_Func()
     {
-        if (Input.GetKeyDown(KeyCode.Z) == false)
-            return;
-
-        //애니메이션 변경 얼마 후 다시 돌아옴
-
-        //슬라이딩 시 아이템 습득 판정 범위가 따라 움직여야 함.
-        //아이템 습득 여부는 이후 캐치 했는지에서 판단할 것.
-    }
-
-    private void Click_PlayerCatch_Func()
-    {
-        if (Input.GetKeyDown(KeyCode.X) == false)
-            return;
-
-        //애니메이션 변경 얼마 후 다시 돌아옴
-
-        //버튼 눌렀을 때 판정 범위 내 오브젝트가 닿았는지 여부를 파악해야 함.
+        this._anim.SetBool("Silde_On", false);
+        this.is_MoveOn = true;
+        this.is_SlidingOn = false;
     }
 
     public Transform Get_ChtchPointTr_Func()
